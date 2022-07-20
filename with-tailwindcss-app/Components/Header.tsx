bda7520908bad5a06b1cd6c52bbc87c0111ae845
callhimsaalim <callhimsaalim@outlook.com>
2022-07-20 22:02:37 +0530

    developed the home-page UI

--- with-tailwindcss-app/Components/Header.tsx
+++ with-tailwindcss-app/Components/Header.tsx
@@ -40,7 +40,7 @@ function Header() {
             <li className="headerLink">Home</li>
             <li className="headerLink">TV Shows</li>
             <li className="headerLink">Movies</li>
-            <li className="headerLink">Hot & New</li>
+            <li className="headerLink">New & Popular</li>
             <li className="headerLink">My List</li>
 
         </ul>
